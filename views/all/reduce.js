2cda94d857654233be8a1e59d9088b583435317d
Motokazu Nihsimura <motokazu.n@gmail.com>
2011-06-16 01:29:26 +0900

    delete couchapp files located old path.

--- views/all/reduce.js
+++
@@ -1,9 +0,0 @@
-function(keys, values){
-	var count = 0;
-	var stime = null;
-	values.forEach(function(val){
-		count += val.count;
-		stime = val.stime;
-	});
-	return {"stime":stime,"count":count};
-}
